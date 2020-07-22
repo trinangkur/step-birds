47e3752115271330a7f6e065b06706cbeec5d75a
Rahit <rahitkar@users.noreply.github.com>
2020-07-22 17:49:13 +0530

    added a test for db added dependency

--- spikes/ci/testServer.js
+++ spikes/ci/testServer.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const assert = require('assert');
+const sqlite3 = require('sqlite3').verbose();
 
 const { app } = require('./app');
 
@@ -10,5 +11,15 @@ describe('home page', () => {
       .expect(200)
       .expect('content-type', /text\/html/)
       .expect(/Welcome to our page/, done);
+  });
+});
+
+describe('db', () => {
+  it('testing query for sqlite with travis', (done) => {
+    const db = new sqlite3.Database('./spikes/db/spike.db');
+    db.get('SELECT * FROM Tweeter WHERE id = "rahit"', (err, row) => {
+      assert.strictEqual(row.id, 'rahit');
+      done();
+    });
   });
 });
