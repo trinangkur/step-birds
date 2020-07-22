93fb63b55a31ea04d98de453c1405e249b8fcd8d
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-07-22 15:49:07 +0530

    modified setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,3 +4,10 @@ chmod +x .git/hooks/pre-commit
 
 cp hooks/pre-push-hook.sh .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
+
+if [ -d "./db" ]; then
+  echo "db directory exists";
+else
+  mkdir db;
+  echo "db directory created";
+fi
