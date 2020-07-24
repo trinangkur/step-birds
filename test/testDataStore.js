9491206a1909e5b4ab536f7464ccd555b8fdf643
Rahit <rahitkar@users.noreply.github.com>
2020-07-24 12:05:58 +0530

    |#1|trinangkur/rahit|added test for addTweeter method of dataStore class

--- test/testDataStore.js
+++ test/testDataStore.js
@@ -1,7 +1,8 @@
-const assert = require('assert');
+const { assert } = require('chai');
+const sinon = require('sinon');
 const sqlite3 = require('sqlite3').verbose();
-const {getDB} = require('../config');
-const {DataStore} = require('../src/models/datastore');
+const { getDB } = require('../config');
+const { DataStore } = require('../src/models/datastore');
 const dbFilePath = getDB();
 
 const db = new sqlite3.Database(dbFilePath);
@@ -15,12 +16,12 @@ describe('postTweet', function() {
     const details = {
       userId: 'vikram',
       content: 'Testing tweet',
-      type: 'tweet'
+      type: 'tweet',
     };
     const status = await dataStore.postTweet(details);
     const tweetDetails = {
       columns: ['content'],
-      where: 'userId="vikram" and content="Testing tweet"'
+      where: 'userId="vikram" and content="Testing tweet"',
     };
     const [tweet] = await dataStore.getTweet(tweetDetails);
     assert.deepStrictEqual(tweet.content, details.content);
@@ -34,15 +35,64 @@ describe('deleteTweet', function() {
 
     const dataStore = new DataStore(db);
     const details = {
-      tweetId: 1
+      tweetId: 1,
     };
     const status = await dataStore.deleteTweet(details);
     const tweetDetails = {
       columns: ['*'],
-      where: 'id=1'
+      where: 'id=1',
     };
     const tweet = await dataStore.getTweet(tweetDetails);
     assert.deepStrictEqual(tweet, []);
     assert.deepStrictEqual(status, []);
+  });
+});
+
+describe('addTweeter', () => {
+  it('should add a new tweeter in database', (done) => {
+    const run = sinon.stub().yields(null);
+    const dataStore = new DataStore({ run });
+    dataStore
+      .addTweeter({
+        login: 'abc',
+        avatar_url: 'https://url',
+        name: 'abc',
+      })
+      .then((message) => {
+        assert.strictEqual(message, 'OK');
+        done();
+      });
+  });
+
+  it('should should handle err of sql constrain', (done) => {
+    const err = new Error();
+    err.code = 'SQLITE_CONSTRAINT';
+    const run = sinon.stub().yields(err);
+    const dataStore = new DataStore({ run });
+    dataStore
+      .addTweeter({
+        login: 'abc',
+        avatar_url: 'https://url',
+        name: 'abc',
+      })
+      .then((message) => {
+        assert.strictEqual(message, 'already have an account');
+        done();
+      });
+  });
+
+  it('should should throw err other than sql constrain', (done) => {
+    const run = sinon.stub().yields(new Error());
+    const dataStore = new DataStore({ run });
+    dataStore
+      .addTweeter({
+        login: 'abc',
+        avatar_url: 'https://url',
+        name: 'abc',
+      })
+      .catch((err) => {
+        assert.isNotNull(err);
+        done();
+      });
   });
 });
