2bb5d10a747884e8fb1361a13eb24a4261b7f42e
Rahit <rahitkar@users.noreply.github.com>
2020-07-24 10:24:46 +0530

    |#1|trinangkur/rahit|added testApp.js for integration
    What changes have you made?
    
    Why do we need this change?
    
    What can this change impact?

---
+++ test/testApp.js
@@ -0,0 +1,26 @@
+const sqlite3 = require('sqlite3');
+const request = require('supertest');
+const { app } = require('../src/js/app');
+const { DataStore } = require('../src/models/datastore');
+const { getDB } = require('../config');
+const db = new sqlite3.Database(getDB());
+app.locals.dataStore = new DataStore(db);
+const sessions = {};
+sessions.createCookie = () => '123';
+app.locals.sessions = sessions;
+
+describe('login page', () => {
+  it('should serve login page when requested for /login.html', (done) => {
+    request(app)
+      .get('/login.html')
+      .expect(200, done);
+  });
+});
+
+describe('loginUser', () => {
+  it('should serve the user login when requested for /loginUser', (done) => {
+    request(app)
+      .get('/loginUser')
+      .expect(302, done);
+  });
+});
