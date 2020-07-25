ab9944c32f4469ae6fcf7a2c14549dcedfc0f901
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-07-25 21:21:12 +0530

    |#3|trinangkur/rahit| added onclick on home and profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,6 @@
 const getUserTweets = function () {
   const url = '/user/getUserTweets';
-  const id = document.querySelector('#user-id').innerText.slice(1);
+  const id = document.querySelector('#profile-id').innerText.slice(1);
   sendPOSTRequest(url, { id }, ({ tweets }) => {
     tweets.forEach((tweet) => {
       showTweet(tweet);
@@ -12,6 +12,11 @@ const getDp = function () {
   const url = '/user/getUserInfo';
   sendGETRequest(url, ({ message, userInfo }) => {
     if (message === 'successful') {
+      document.querySelector('#user-id').innerText = `@${userInfo[0].id}`;
+      const profiles = document.getElementsByClassName('profileIcon');
+      Array.from(profiles).forEach((profile) => {
+        profile.src = userInfo[0].image_url;
+      });
       const profileIcon = document.querySelector('#profile-icon');
       profileIcon.innerHTML = `
       <img class="profile-pic" src="${userInfo[0].image_url}"/>`;
